bb6f0a583066dc233772a5986d3fff118e341796
DuanHuiK <147907725+DuanHuiK@users.noreply.github.com>
2023-10-27 20:10:25 +0200

    Add files via upload

---
+++ coordinates.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Coordinates
+//
+//  Created by Duan Huikai on 21/10/2023.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(int argc, const char * argv[]) {
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    int a;
+    int b;
+    a=
+    printf("");
+    return 0;
+}
