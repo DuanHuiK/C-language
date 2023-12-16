223b14729ccf96d50ce6f6d66b15ced7d54f2a38
DuanHuiK <147907725+DuanHuiK@users.noreply.github.com>
2023-12-16 13:20:54 +0100

    Add files via upload

---
+++ function.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  function
+//
+//  Created by Duan Huikai on 16/12/2023.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    double a;
+    a=-1;
+    for (a=-1; a<1; a=a+0.1)
+    {
+        printf("%.4f", a );
+    }
+    return 0;
+}
+
+//for (a=-1; a<1; a=a+0.1)
+//
+//printf("%.4f",a);
+//  printf("%.4f",sin (a));
