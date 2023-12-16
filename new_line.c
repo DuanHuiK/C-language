a373c062c391e3c3a3644859b900c2a73617e8f0
DuanHuiK <147907725+DuanHuiK@users.noreply.github.com>
2023-12-16 13:12:42 +0100

    Add files via upload

---
+++ new_line.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  n
+//
+//  Created by Duan Huikai on 16/12/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("  /\n /\n/\n");
+    //printf("\\\n \\\n  \\");
+    return 0;
+}
